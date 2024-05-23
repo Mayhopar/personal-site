4dc09ecbbf3811af740f05722d665c7ab5afb7c8
Mayhopar <oleg.mayhopar@gmail.com>
2024-05-23 11:15:00 +0200

    added prefetch all on load by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  prefetch: {
+    defaultStrategy: 'load',
+    prefetchAll: true
+  }
 });
